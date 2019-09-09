bb8c74755608d26df115273c60b12163e933328e
Chris <chris@chrisvoncsefalvay.com>
2019-09-09 21:22:29 +0200

    Hotpatching project name into navbar.

--- {{cookiecutter.repo_name}}/app.py
+++ {{cookiecutter.repo_name}}/app.py
@@ -25,7 +25,7 @@ app = dash.Dash(__name__,
 
 header = """
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">{{}}</a>
+        <a class="navbar-brand" href="#">{{cookiecutter.project_name}}</a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" 
           aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
