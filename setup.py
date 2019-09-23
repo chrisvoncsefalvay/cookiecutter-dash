c3684eb7f73a77b4cb2366e37a8ccd8eafbe4a65
Chris <chris@chrisvoncsefalvay.com>
2019-09-23 09:52:23 -0700

    Exempted all .idea files and created a setup file for version etc. tracking.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cookiecutter-dash',
+    version='1.1.3',
+    packages=[''],
+    url='https://github.com/chrisvoncsefalvay/cookiecutter-dash',
+    license='Apache-2.0',
+    author='Chris von Csefalvay',
+    author_email='chris@chrisvoncsefalvay.com',
+    description='Easily generate a starter project for Plotly Dash.'
+)
